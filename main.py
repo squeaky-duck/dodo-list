6b174260ab6c3a17b79ae9d1b32ecf12009656d5
sqeaky-duck <mehulj35@gmail.com>
2023-05-21 03:35:43 +0530

    Add time label feature

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 # from functions import get_todos, write_todos
 import functions
+import time
+
+now = time.strftime("%b %d, %Y %H:%M:%S")
+print("Time: ", now)
+
 while True:
     user_action = input("Type add, show, edit, complete, or exit: ").strip()
     if user_action.startswith('add'):
