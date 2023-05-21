4bf01479215bd834460859013c18cf30a4eddbcb
sqeaky-duck <mehulj35@gmail.com>
2023-05-22 02:22:59 +0530

    Implement the Complete and Exit buttons

--- gui.py
+++ gui.py
@@ -8,14 +8,18 @@ list_box = sg.Listbox(values=functions.get_todos(), key="todos",
                       enable_events=True, size=(45, 10))
 edit_button = sg.Button("Edit")
 
+complete_button = sg.Button("Complete")
+
+exit_button = sg.Button("Exit")
+
 window = sg.Window('DoDo List',
-                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
+                   layout=[[label], [input_box, add_button],
+                           [list_box, edit_button, complete_button],
+                           [exit_button]],
                    font=('Helvetica', 20))
 
 while True:
     event, values = window.read()
-    print(event)
-    print(values)
     match event:
         case "Add":
             todos = functions.get_todos()
@@ -35,8 +39,18 @@ while True:
             window['todos'].update(values=todos)
 
         case 'todos':
-            window['todo'].update(value=values['todos'])
+            window['todo'].update(value=values['todos'][0])
 
+        case 'Complete':
+            todo_to_complete = values['todos'][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value="")
+
+        case "Exit":
+            break
         case sg.WIN_CLOSED:
             break
 
