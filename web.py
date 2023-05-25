582fd372e64a9ca0cdbe7f95ce698ccef3ac3d70
sqeaky-duck <mehulj35@gmail.com>
2023-05-26 00:06:19 +0530

    Implement complete todo feature in web app

--- web.py
+++ web.py
@@ -14,8 +14,13 @@ st.title("The DoDo List")
 st.subheader("A todo app for silly ducks")
 st.write("Duck forgets, duck notes it down")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
 
 st.text_input(label="", placeholder="Add new todo...",
               on_change=add_todo, key='new_todo')
