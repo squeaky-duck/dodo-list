ceeaaf24d228899ade8ee89ceb7e36bdf4374418
sqeaky-duck <mehulj35@gmail.com>
2023-05-25 23:58:30 +0530

    Implement add todo feature in web app

--- web.py
+++ web.py
@@ -3,6 +3,13 @@ import functions
 
 todos = functions.get_todos()
 
+
+def add_todo():
+    new_todo = st.session_state["new_todo"] + "\n"
+    todos.append(new_todo)
+    functions.write_todos(todos)
+
+
 st.title("The DoDo List")
 st.subheader("A todo app for silly ducks")
 st.write("Duck forgets, duck notes it down")
@@ -10,4 +17,5 @@ st.write("Duck forgets, duck notes it down")
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label="", placeholder="Add new todo...")
+st.text_input(label="", placeholder="Add new todo...",
+              on_change=add_todo, key='new_todo')
